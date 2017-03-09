f66eabcbeeb106ab8c796089301a6c6104706008
Maciej Nowakowski <macnow@gmail.com>
2017-03-09 01:10:27 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -3,6 +3,6 @@
 sudo apt-get update
 
 sudo apt-get install -y git
-git clone https://github.com/macnow/homebridge-raspbian-installer.git
+git clone -b testing https://github.com/macnow/homebridge-raspbian-installer.git
 cd homebridge-raspbian-installer
 ./install.sh
