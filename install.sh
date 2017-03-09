b83df2b9820336bd5de64b8195e39f8da5028834
Maciej Nowakowski <macnow@gmail.com>
2017-03-09 00:05:13 +0000

    testing

--- install.sh
+++ install.sh
@@ -36,19 +36,18 @@ npm config set prefix '~/.npm-global'
 export PATH=~/.npm-global/bin:$PATH
 source ~/.profile
 
-npm install -g --unsafe-perm homebridge hap-nodejs node-gyp json homebridge-server
-cd ~/.npm-global/homebridge
+npm install -g --unsafe-perm node-gyp homebridge hap-nodejs json homebridge-server
+cd ~/.npm-global/lib/node_modules/homebridge
 npm install --unsafe-perm bignum
 cd -
-cd ~/.npm-global/hap-nodejs/node_modules/mdns
-sudo node-gyp BUILDTYPE=Release rebuild
+cd ~/.npm-global/lib/node_modules/hap-nodejs/node_modules/mdns
+node-gyp BUILDTYPE=Release rebuild
 cd -
 sudo useradd --system --user-group homebridge
 sudo usermod -a -G systemd-journal homebridge
 
 sudo mkdir -p /var/homebridge
 sudo cp configs/config.json /var/homebridge/config.json
-#printf '%02X:%02X:%02X:%02X:%02X:%02X\n' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256] $[RANDOM%256] $[RANDOM%256] $[RANDOM%256]
 sudo chown -R homebridge:homebridge /var/homebridge/
 
 sudo cp files/homebridge /etc/default/homebridge
